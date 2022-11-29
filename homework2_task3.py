5c8c9212a3a9e05975260b7424e82f2ffcf54951
Kuznetsov Vadim <kuzibundius@gmail.com>
2022-11-29 22:45:44 +0700

    finish

---
+++ homework2_task3.py
@@ -0,0 +1,11 @@
+# Задайте словарь из n чисел последовательности (1 + (1/n))^n и выведите на экран их сумму
+
+num = int(input('Введите число: '))
+lst = {}
+total = 0
+for i in range(1, num + 1):
+    lst[i] = round((1 + (1 / i)) ** i, 3)
+    total += lst[i]
+print(lst)
+print(f'Сумма = {round(total, 3)}')
+
---
+++ homework2_task4.py
@@ -0,0 +1,18 @@
+# Задайте список из N элементов, заполненных числами из промежутка [-N, N].
+# Найдите произведение элементов на указанных позициях.
+# Позиции хранятся в файле file.txt в одной строке одно число.
+
+num = int(input('Введите N: '))
+lst = []
+for i in range(-abs(num), abs(num) + 1):
+    lst.append(i)
+print(lst)
+mult = 1
+with open('position.txt', 'r') as data:
+    for i in data:
+        if int(i) > len(lst) - 1 or int(i) < -len(lst):
+            print('Неверный индекс')
+            exit()
+        mult *= lst[int(i)]
+print(f'Произведение элементов = {mult}')
+
